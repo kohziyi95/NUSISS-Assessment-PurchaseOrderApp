df4f97588986261f02631a3a1da32b2b9eca3789
kohziyi95 <96542810+kohziyi95@users.noreply.github.com>
2022-03-25 11:45:58 +0800

    new

--- src/main/java/sg/edu/nus/iss/assessment/purchaseorderapp/controller/PurchaseOrderRestController.java
+++ src/main/java/sg/edu/nus/iss/assessment/purchaseorderapp/controller/PurchaseOrderRestController.java
@@ -7,8 +7,9 @@ import java.util.List;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,20 +17,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.json.Json;
 import jakarta.json.JsonObject;
+import jakarta.json.JsonObjectBuilder;
 import jakarta.json.JsonReader;
+import sg.edu.nus.iss.assessment.purchaseorderapp.model.Quotation;
+import sg.edu.nus.iss.assessment.purchaseorderapp.service.QuotationService;
 
 @RestController
-@RequestMapping(path="/")
+@RequestMapping(path="/api", consumes="application/json")
 public class PurchaseOrderRestController {
     private static Logger logger = Logger.getLogger(PurchaseOrderRestController.class.getName());
 
-    @PostMapping(consumes="application/json")
+    @Autowired
+    private QuotationService service;
+
+    @PostMapping(path="/po", consumes="application/json")
     public ResponseEntity<String> showQuotation(@RequestBody String payload){
         JsonObject body;
         try (InputStream is = new ByteArrayInputStream(payload.getBytes())){
             JsonReader reader = Json.createReader(is);
             body = reader.readObject();
+            logger.log(Level.INFO, "Payload secured");
         } catch (Exception e) {
+            logger.log(Level.INFO, "Unable to get payload");
             body = Json.createObjectBuilder().add("error", e.getMessage()).build();
         }
         
@@ -41,8 +50,28 @@ public class PurchaseOrderRestController {
         }  
         logger.log(Level.INFO, poList.toString());
 
-        return null;
+        Quotation quotation = service.getQuotations(poList).get();
 
+        Float totalAmount = (float) 0;
+        for (String items : poList){
+            JsonObject item = (JsonObject) body.getJsonArray("lineItems")
+                    .stream()
+                    .filter(v-> ((JsonObject) v).get("item").equals(items))
+                    .findFirst()
+                    .get();
+            int numberOfItems = item.getInt("quantity");
+            Float cost = quotation.getQuotation(items) * numberOfItems;
+            logger.log(Level.INFO, items + "  qty: " + numberOfItems + " >>>>  $" + cost);
+            totalAmount += cost;
+        }
+
+        JsonObject result = Json.createObjectBuilder()
+                .add("invoiceId", quotation.getQuoteId())
+                .add("name", body.getString("name"))
+                .add("total", totalAmount)
+                .build();
+
+        return ResponseEntity.ok(result.toString());
     }
     
 }
--- src/main/java/sg/edu/nus/iss/assessment/purchaseorderapp/service/QuotationService.java
+++ src/main/java/sg/edu/nus/iss/assessment/purchaseorderapp/service/QuotationService.java
@@ -4,7 +4,9 @@ package sg.edu.nus.iss.assessment.purchaseorderapp.service;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.logging.Logger;
 
@@ -13,6 +15,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import jakarta.json.Json;
@@ -26,6 +29,7 @@ import java.util.logging.Level;
 
 import sg.edu.nus.iss.assessment.purchaseorderapp.model.Quotation;
 
+@Service
 public class QuotationService {
     private Logger logger = Logger.getLogger(QuotationService.class.getName());
 
@@ -40,22 +44,29 @@ public class QuotationService {
         String url = "https://quotation.chuklee.com";
 
         HttpHeaders headers = new HttpHeaders();
-        // set `content-type` header
         headers.setContentType(MediaType.APPLICATION_JSON);
-        // set `accept` header
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        logger.log(Level.INFO, "Headers >>> " + headers.toString());
+
 
         HttpEntity<JsonArray> entity = new HttpEntity<>(jsonArray, headers);
         
         RestTemplate template = new RestTemplate();
 
         ResponseEntity<String> response = template.postForEntity(url, entity, String.class);
+        
+        logger.log(Level.INFO, "response status >>>> " + response.getStatusCode().toString());
+
+        
         JsonObject data = null;
 
         if (response.getStatusCode() == HttpStatus.CREATED) {
             InputStream is = new ByteArrayInputStream(response.getBody().getBytes());
             JsonReader reader = Json.createReader(is);
             data = reader.readObject();
+            logger.log(Level.INFO, "HTTP created");
+
 
         } else {
             logger.log(Level.SEVERE, "Fail to get response");
@@ -65,14 +76,16 @@ public class QuotationService {
         Quotation quotation = new Quotation();
         quotation.setQuoteId(data.getString("quoteId"));
         // quotation.setQuotations("quotations");
+        JsonObject quoteData = data.getJsonObject("quotations");
+        // quoteData.keySet();
+        // Iterator<String> itr = quoteData.keySet().iterator();
+        for (String key : quoteData.keySet()){
+            quotation.addQuotation(key, Float.parseFloat(quoteData.get(key).toString()));
+        }
 
+        Optional<Quotation> opt = Optional.of(quotation);
 
-
-
-
-        
-
-
+        return opt;
 
     }
 
